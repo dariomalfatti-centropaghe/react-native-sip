bc0f40e23bf09cb8649e586e44bda93ac68facc2
dariomalfatti-centropaghe <dario.m@centropaghe.it>
2020-10-13 14:05:44 +0200

    create node js script to generate docker containers

---
+++ libs.js
@@ -0,0 +1,23 @@
+const { spawn } = require('child_process');
+
+
+async function main() {
+
+    console.log("Execute release.js");
+    const script = spawn('node ./react-native-pjsip-builder-2.8.0/release.js', { shell: true });
+
+    for await (const data of script.stdout) {
+        console.log(`${data}`);
+    }
+
+    const fs = require('fs-extra');
+
+    console.log("Copy iOS stuff");
+    fs.copySync(__dirname + '/react-native-pjsip-builder-2.8.0/dist/ios/VialerPJSIP.framework',  __dirname + '/ios/', { overwrite: true });
+
+    console.log("Copy Android stuff");
+    fs.copySync(__dirname + '/react-native-pjsip-builder-2.8.0/dist/android/src/', __dirname + '/android/src/', { overwrite: true });
+
+}
+
+main();
