edefefdef249bac8808e81ea942b188752ef1adc
dariomalfatti-centropaghe <dario.m@centropaghe.it>
2020-10-13 16:04:02 +0200

    handle to create a lock file

--- libs.js
+++ libs.js
@@ -3,6 +3,13 @@ const { spawn } = require('child_process');
 
 async function main() {
 
+    const file = require('fs');
+
+    if (file.existsSync(__dirname + '/.libs.lock')) {
+        console.log('File .libs.lock already exists, exit');
+        return;
+    }
+
     console.log("Execute release.js");
     const script = spawn('node ./react-native-pjsip-builder-2.8.0/release.js', { shell: true });
 
@@ -17,6 +24,10 @@ async function main() {
 
     console.log("Copy Android stuff");
     fs.copySync(__dirname + '/react-native-pjsip-builder-2.8.0/dist/android/src/', __dirname + '/android/src/', { overwrite: true });
+
+
+    console.log("Write .libs.lock file");
+    file.writeFileSync(__dirname + '/.libs.lock', 'libs.lock');
 
 }
 
